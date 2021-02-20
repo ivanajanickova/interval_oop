0e2e4d1ebb69561d5c4261f16a6acbeb667e0e3d
ivanajanickova <ivana.janickova97@gmail.com>
2021-02-20 17:49:36 +0100

    Implementation using width and upperBound

--- Interval_oop/src/Interval_oop/Interval.java
+++ Interval_oop/src/Interval_oop/Interval.java
@@ -16,50 +16,71 @@ import java.util.stream.IntStream;
 
 
 public class Interval {	
-		//getters (inspector methods) --- defines the class's abstract state space = set of possible abstract values/states)
-		//immutable class --> the abstract value of an instance
-		//Mutable class --> the current abstract state of an instance 
-		
-		/**
-		 * @basic
-		 */
-		public int getLowerBound() {  throw new RuntimeException("Not yet implemented");}
-		
-		/**
-		 * @basic
-		 */
-		public int getUpperBound() { throw new RuntimeException("Not yet implemented"); }
-		
-		/**
-		 * @post The result equals the upper bound minus the lower bound.
-		 * 		| result == getUpperBound() - getLowerBound()
-		 */
-		public int getWidth() { throw new RuntimeException("Not yet implemented"); }
-		
-		/**
-		 * @post The result is not null
-		 * 		| getElements() != null
-		 * @post The sequence of elements equals the sequence starting at he lower bound
-		 * 		| Arrays.equals(getElements(), IntStream.range(getLowerBound(), getUpperBound()))
-		 */
-		public int[] getElements() { throw new RuntimeException("Not yet implemented"); }
-		
-		/**
-		 * Initializes this object so that it represents the interval
-		 * specified by the given lower and upper bound
-		 * 
-		 * @pre The given lower bound is not grater that the given upper bound.
-		 * 		| lowerBound <= upperBound
-		 * @post This interval's lower bound equals the given lower bound.
-		 * 		| getLowerBound() == lowerBound
-		 * @post This interval's upper bound equals the given lower bound.
-		 * 		| getUpperBound() == upperBound
-		 * 
-		 */
-		
-		public Interval(int lowerBound, int upperBound) { throw new RuntimeException("Not yet implemented"); }
-		//this would be immutable class (no setters) - no way for clients to change the abstract state of object == no Mutators
-		
+	
+	//representation invariants a.k.a. private invariants 
+	/**
+	 * @invar The with is nonegative.
+	 * 		| 0 <= width
+	 */
+	
+	private int width;
+	private int upperBound;
+	
+	//getters (inspector methods) --- defines the class's abstract state space = set of possible abstract values/states)
+	//immutable class --> the abstract value of an instance
+	//Mutable class --> the current abstract state of an instance 
+	
+	/**
+	 * @basic
+	 */
+	public int getLowerBound() {  
+		return upperBound - width;
+		}
+	
+	/**
+	 * @basic
+	 */
+	public int getUpperBound() { 
+		return upperBound;
+		}
+	
+	/**
+	 * @post The result equals the upper bound minus the lower bound.
+	 * 		| result == getUpperBound() - getLowerBound()
+	 */
+	public int getWidth() { 
+		return width;
+		}
+	
+	/**
+	 * @post The result is not null
+	 * 		| result != null
+	 * @post The sequence of elements equals the sequence starting at he lower bound
+	 * 		| Arrays.equals(result, IntStream.range(getLowerBound(), getUpperBound()).toArray())
+	 */
+	public int[] getElements() {
+		return IntStream.range(upperBound - width, upperBound).toArray();
+		}
+	
+	/**
+	 * Initializes this object so that it represents the interval
+	 * specified by the given lower and upper bound
+	 * 
+	 * @pre The given lower bound is not grater that the given upper bound.
+	 * 		| lowerBound <= upperBound
+	 * @post This interval's lower bound equals the given lower bound.
+	 * 		| getLowerBound() == lowerBound
+	 * @post This interval's upper bound equals the given lower bound.
+	 * 		| getUpperBound() == upperBound
+	 * 
+	 */
+	
+	public Interval(int lowerBound, int upperBound) { 
+		this.width = upperBound - lowerBound;
+		this.upperBound = upperBound;
+		}
+	//this would be immutable class (no setters) - no way for clients to change the abstract state of object == no Mutators
+	
 
 	
 
--- Interval_oop/src/Interval_oop/tests/Interval_oopTest.java
+++ Interval_oop/src/Interval_oop/tests/Interval_oopTest.java
@@ -12,9 +12,9 @@ class Interval_oopTest {
 	void test() {
 		Interval myInterval = new Interval(4,7);
 		assertEquals(4, myInterval.getLowerBound());
-		assertEquals(4, myInterval.getUpperBound());
-		assertEquals(4, myInterval.getWidth());
-		assertArrayEquals(new int[] {4, 5, 6, 7}, myInterval.getElements());
+		assertEquals(7, myInterval.getUpperBound());
+		assertEquals(3, myInterval.getWidth());
+		assertArrayEquals(new int[] {4, 5, 6}, myInterval.getElements());
 	}
 
 }
